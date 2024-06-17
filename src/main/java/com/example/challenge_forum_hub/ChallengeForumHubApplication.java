2ffb7e58b0f780f7fd86eb4408d6feea32bd1c7a
Victor olv <victorolvdev@gmail.com>
2024-06-17 10:26:16 -0300

    first commit

---
+++ src/main/java/com/example/challenge_forum_hub/ChallengeForumHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.challenge_forum_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumHubApplication.class, args);
+	}
+
+}
