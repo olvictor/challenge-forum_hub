f0a27ec0bff4420e1be061e896d0cce0c807c705
Victor olv <victorolvdev@gmail.com>
2024-07-03 16:20:36 -0300

    adicao do metodo UserDetails na classe Usuario

--- src/main/java/com/example/challenge_forum_hub/domain/Usuario/Usuario.java
+++ src/main/java/com/example/challenge_forum_hub/domain/Usuario/Usuario.java
@@ -10,6 +10,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
@@ -17,11 +22,46 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private long Id;
    private String nome;
    private String email;
    private String senha;
+
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      return List.of();
+   }
+
+   @Override
+   public String getPassword() {
+      return senha;
+   }
+
+   @Override
+   public String getUsername() {
+      return email;
+   }
+
+   @Override
+   public boolean isAccountNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isAccountNonLocked() {
+      return true;
+   }
+
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isEnabled() {
+      return true;
+   }
 }
