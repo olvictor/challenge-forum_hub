e8e58e5efc32f62c3a67fac7b1dcfc97ed9df103
Victor olv <victorolvdev@gmail.com>
2024-07-03 16:37:51 -0300

    criacao do servico de authenticacao

---
+++ src/main/java/com/example/challenge_forum_hub/service/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.example.challenge_forum_hub.service;
+
+import com.example.challenge_forum_hub.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthenticationService implements UserDetailsService {
+    @Autowired UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email);
+    }
+}
